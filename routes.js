d7565f90fc534443afbd3335f6540b2618836de5
Wagner Rodrigues <wagnersr92@gmail.com>
2019-06-10 00:39:00 -0300

    Criação de schema Ad e inicio criação crud

--- routes.js
+++ routes.js
@@ -3,12 +3,20 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const controllers = require('./app/controllers')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
 
-routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
+// routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
+
+// daqui pra baixo, só aceita se estiver logado
+routes.use(authMiddleware)
+
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes
